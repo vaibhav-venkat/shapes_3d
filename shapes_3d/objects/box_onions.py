19a24f8cde88b6408e4d05e861e0d4a01a1889a7
vibovenkat123 <vibovenkat@gmail.com>
2025-03-17 13:00:28 -0500

    chore: fix names

--- shapes_3d/objects/box_onions.py
+++ shapes_3d/objects/box_onions.py
@@ -3,38 +3,47 @@ from ..modules.onion import Onion
 from pathlib import Path
 from ..modules.utils import save_dump, make_centers
 
-ts = np.array([10.0, 7.0, 6.0, 5.0, 4.0])
-std_ts = np.array([1.5, 1.2, 0.5, 0.8, 1.0])
+thickness_mean = np.array([10.0, 7.0, 6.0, 5.0, 4.0])
+thickness_std = np.array([1.5, 1.2, 0.5, 0.8, 1.0])
 density = np.array([0.0, 0.05, 0.1, 0.03, 0.2])
-L: float = 800
-VOL_FR: float = 0.05
-assert density.shape == std_ts.shape == ts.shape
+box_length: float = 800
+volume_fraction: float = 0.05
+assert density.shape == thickness_std.shape == thickness_mean.shape
 
 radii = []
-tgt = (L**3) * VOL_FR
-curr_vol: float = 0
-sigma_ts: np.ndarray = np.sqrt(np.log(1 + (std_ts / ts) ** 2))
-mu_ts: np.ndarray = np.log(ts) - sigma_ts**2 / 2
-while curr_vol < tgt:
-    rad_tot: float = 0
-    curr_sphere = np.random.lognormal(mu_ts, sigma_ts)
-    curr_vol += float(np.sum(curr_sphere) ** 3) * np.pi * 4 / 3
-    if curr_vol > tgt:
+target = (box_length**3) * volume_fraction
+current_volume: float = 0
+log_thickness_std: np.ndarray = np.sqrt(
+    np.log(1 + (thickness_std / thickness_mean) ** 2)
+)
+log_thickness_mean: np.ndarray = np.log(thickness_mean) - log_thickness_std**2 / 2
+while current_volume < target:
+    total_radius: float = 0
+    curr_sphere = np.random.lognormal(log_thickness_mean, log_thickness_std)
+    current_volume += float(np.sum(curr_sphere) ** 3) * np.pi * 4 / 3
+    if current_volume > target:
         break
     radii.append(curr_sphere)
 radii = np.array(radii)
 N: int = radii.shape[0]
 
 
-max_r: float = np.max(radii.sum(axis=1))
-centers = make_centers(N, -L / 2 + max_r, L / 2 - max_r, 2 * max_r)
-pts = []
+max_total_radius: float = np.max(radii.sum(axis=1))
+centers = make_centers(
+    N,
+    -box_length / 2 + max_total_radius,
+    box_length / 2 - max_total_radius,
+    2 * max_total_radius,
+)
+
+
+points = []
 for i in range(N):
     if (i + 1) % 100 == 0 or i == 0 or i == N - 1:
         print("N =", i + 1, "out of", N)
     shell = Onion(radii[i], centers[i], density)
-    pts.extend(shell.pts)
-pts = np.array(pts)
+    points.extend(shell.pts)
+points = np.array(points)
 
 
 def save_coords(points: np.ndarray, filename: str = "out.txt") -> None:
@@ -46,5 +55,5 @@ def save_coords(points: np.ndarray, filename: str = "out.txt") -> None:
     print("saved to", filename)
 
 
-save_coords(pts, "out/box_onion.txt")
-save_dump([pts], "out/box_onion.dump", L)
+save_coords(points, "out/box_onion.txt")
+save_dump([points], "out/box_onion.dump", box_length)
