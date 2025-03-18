4b28408181668b7745cb9dae2e07df65aa17ffd1
vibovenkat123 <vibovenkat@gmail.com>
2025-03-17 19:06:59 -0500

    feat: allow for 3 axis variations on the ellipsoid

--- shapes_3d/modules/ellipsoid.py
+++ shapes_3d/modules/ellipsoid.py
@@ -13,6 +13,10 @@ class Ellipsoid:
         The outer radius of the x axis
     x_inner_radius : float | None
         The inner radius of the x axis
+    y_outer_radius : float
+        The outer radius of the y axis
+    y_inner_radius : float | None
+        The inner radius of the y axis
     z_outer_radius : float | None
         The z axis outer radius
     z_inner_radius : float | None
@@ -24,6 +28,8 @@ class Ellipsoid:
         density: float,
         x_outer_radius: float,
         x_inner_radius: float | None = None,
+        y_outer_radius: float | None = None,
+        y_inner_radius: float | None = None,
         z_outer_radius: float | None = None,
         z_inner_radius: float | None = None,
     ):
@@ -34,33 +40,26 @@ class Ellipsoid:
         ----------
         density : float
             The uniform density of the ellipsoid
-        x_outer_radius : float | None
+        x_outer_radius : float
             The outer radius of the x axis
         x_inner_radius : float | None
             The inner radius of the x axis
+        y_outer_radius : float
+            The outer radius of the y axis
+        y_inner_radius : float | None
+            The inner radius of the y axis
         z_outer_radius : float | None
             The z axis outer radius
         z_inner_radius : float | None
             The z axis inner radius
         """
         self.density: float = density
-        if x_inner_radius is None:
-            self.x_inner_radius: float = 0
-        else:
-            self.x_inner_radius: float = x_inner_radius
-
         self.x_outer_radius: float = x_outer_radius
-
-        if (
-            z_outer_radius == x_outer_radius
-            or z_outer_radius is None
-            or z_inner_radius is None
-        ):
-            self.z_outer_radius: float = x_outer_radius
-            self.z_inner_radius: float = self.x_inner_radius
-        else:
-            self.z_outer_radius: float = z_outer_radius
-            self.z_inner_radius: float = z_inner_radius
+        self.x_inner_radius: float | None = x_inner_radius
+        self.y_outer_radius: float | None = y_outer_radius
+        self.y_inner_radius: float | None = y_inner_radius
+        self.z_outer_radius: float | None = z_inner_radius
+        self.z_inner_radius: float | None = z_inner_radius
 
     def make_obj(self) -> np.ndarray:
         """
@@ -71,21 +70,22 @@ class Ellipsoid:
         np.ndarray
             The points of the ellispoid in the 3d plane
         """
-
-        max_radius: float = max(self.x_outer_radius, self.z_outer_radius)
+        x_outer: float = self.x_outer_radius
+        y_outer: float = self.y_outer_radius or x_outer
+        z_outer: float = self.z_outer_radius or x_outer
+        x_inner: float = self.x_inner_radius or 0
+        y_inner: float = self.y_inner_radius or x_inner
+        z_inner: float = self.z_inner_radius or x_inner
+        max_radius: float = max(x_outer, y_outer, z_outer)
         num_points: int = int(self.density * (2 * max_radius) ** 3)
         points: np.ndarray = np.random.uniform(
             low=-max_radius, high=max_radius, size=(num_points, 3)
         )
-        norm_pts: np.ndarray = points / np.array(
-            [self.x_outer_radius, self.x_outer_radius, self.z_outer_radius]
-        )
-        if self.x_inner_radius == 0:
+        norm_pts: np.ndarray = points / np.array([x_outer, y_outer, z_outer])
+        if x_inner == 0:
             norm_i = np.ones(shape=points.shape)
         else:
-            norm_i = points / np.array(
-                [self.x_inner_radius, self.x_inner_radius, self.z_inner_radius]
-            )
+            norm_i = points / np.array([x_inner, y_inner, z_inner])
         distance_outer: float = np.sum(norm_pts**2, axis=1)
         distance_inner: float = np.sum(norm_i**2, axis=1)
         inside = (distance_outer <= 1) & (distance_inner >= 1)
--- shapes_3d/objects/ellipsoidal_obj.py
+++ shapes_3d/objects/ellipsoidal_obj.py
@@ -1,9 +1,13 @@
 from ..modules.ellipsoid import Ellipsoid
 from ..modules.utils import save_dump
 
-x_length = 40
-z_length = 70
-density = 0.02
-ellipsoid = Ellipsoid(density, -x_length, 0, z_length).make_obj()
+a = 40
+b = 30
+c = 80
 
-save_dump([ellipsoid], "out/ellipsoid.dump", 2 * max(x_length, z_length))
+density = 0.02
+ellipsoid = Ellipsoid(
+    density, x_outer_radius=a, y_outer_radius=b, z_outer_radius=c
+).make_obj()
+
+save_dump([ellipsoid], "out/ellipsoid.dump", 2 * max(a, b, c))
